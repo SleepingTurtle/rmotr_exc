b9c85e2dee602752b3cbbafe477cd9977e96fe72
SleepingTurtle <joshcharlestechie@gmail.com>
2017-01-31 22:54:33 -0500

    working on Advanced OOP exercises.

--- colorMixer.py
+++ colorMixer.py
@@ -1,5 +1,5 @@
 def color_mixer(color1, color2):
-    if (color1 == ('red' or 'blue')) and (color2 == ('red' or 'blue')):
+    if (color1 == 'red' or 'blue')) and (color2 == ('red' or 'blue')):
         return 'Magenta'
     elif (color1 == ('yellow' or 'blue')) and (color2 == ('yellow' or 'blue')):
         return 'Green'
--- cookieFactory.py
+++ cookieFactory.py
@@ -1,8 +1,10 @@
 class Cookie:
-    def __init__(self, cookie):
-        self.cookie = cookie
+    def __init__(self):
+        pass
 
     @classmethod
-    def create_cookies(cls):
-        for obj in range(0, cls):
-            .Cookie (obj)
+    def create_cookies(cls, cookies_count):
+        baking_sheet = []
+        for obj in range(0, cookies_count):
+            baking_sheet.append(Cookie())
+        return baking_sheet
